bac7f13b958eda8a5776656f4dbca2bc5ee09ca4
Musa Cihangir <musacihangir8@hotmail.com>
2020-07-22 14:44:46 +0300

    Musa Cihangir tarafından kodlanmıştır

---
+++ guess my number.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int sayi;
+    printf("Merhaba.1 ile 10 arasinda bir sayi giriniz\n");
+
+    while(1)
+    {
+    scanf("%d",&sayi);
+
+    if(sayi!=9)
+        {printf("bilemediniz\n");}
+
+
+    else
+
+        {printf("\nbildiniz.oyunumuzu oynadiginiz icin tesekkur ederiz"); break; }
+
+    }
+
+    return 0;
+
+
+    }
